2243f6786a5f2b35346188322e8395e7611b6687
TayaL <tayalyapina555@gmail.com>
2022-12-02 07:47:16 +0300

    Files Task41 and task 43 added.

---
+++ Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+Console.Write("Введите числа через запятую: ");
+string output = Console.ReadLine();
+
+char symbol = ',';
+string[] subs = output.Split(symbol, StringSplitOptions.RemoveEmptyEntries);
+
+int [] numbers = Array.ConvertAll(subs, int.Parse);
+
+int count = 0;
+
+for (int i = 0; i< numbers.Length; i++)
+{
+    if (numbers[i]>0)
+        count++;
+        Console.Write($" {numbers[i]} | ");
+}
+
+ Console.WriteLine($" - > {count}");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double numberB1 = ReadInt("Введите число b1: ");
+double numberK1 = ReadInt("Введите число k1: ");
+double numberB2 = ReadInt("Введите число b2: ");
+double numberK2 = ReadInt("Введите число k2: ");
+
+if (numberB1 == numberB2)
+    Console.WriteLine("Прямые совпадают");
+
+else if(numberK1 != numberK2)
+{
+    double resultX = (numberB2-numberB1)/(numberK1-numberK2);
+    double resultY = numberK1 * resultX + numberB1;
+    Console.WriteLine($"Точка пересечения двух прямых ({resultX}; {resultY})");
+}
+else
+    Console.WriteLine("Прямые параллельны");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
